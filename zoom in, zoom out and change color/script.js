109c7cabcb6694315bc6c533f28fc41dc9ee8129
Damian Cygan <damiancygan22@gmail.com>
2023-10-16 23:53:46 +0200

    finising project

--- zoom in, zoom out and change color/script.js
+++ zoom in, zoom out and change color/script.js
@@ -6,17 +6,27 @@ const text = document.querySelector('p');
 let textSize = 36;
 
 const bigger = () => {
+	if (textSize >= 54) {
+		return;
+	}
 	textSize += 6;
 	text.style.fontSize = textSize + 'px';
 };
 
 const smaller = () => {
+	if (textSize <= 18) {
+		return;
+	}
 	textSize -= 6;
 	text.style.fontSize = textSize + 'px';
 };
 
 const changeColor = () => {
-	text.style.color = 'gold';
+	const colorNumberFirst = Math.floor(Math.random() * 255);
+	const colorNumberSecond = Math.floor(Math.random() * 255);
+	const colorNumberThird = Math.floor(Math.random() * 255);
+
+	text.style.color = `rgb(${colorNumberFirst},${colorNumberSecond},${colorNumberThird})`;
 };
 
 sizeUp.addEventListener('click', bigger);
