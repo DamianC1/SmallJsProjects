488f1e0c5e9b7764913924739c272d2a7d2b7d06
Damian Cygan <damiancygan22@gmail.com>
2023-10-17 16:17:36 +0200

    finished project

--- password validator/script.js
+++ password validator/script.js
@@ -4,3 +4,36 @@ const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
+
+const show = () => {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		p.textContent = 'Masz świetne hasło';
+		p.style.color = 'lime';
+	} else if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers)
+	) {
+		p.textContent = 'Masz średnie hasło';
+		p.style.color = 'yellow';
+	} else {
+		p.textContent = 'Masz słabe hasło';
+		p.style.color = 'red';
+	}
+};
+
+const checkPassword = () => {
+	if (pass.value !== '') {
+		show();
+	} else {
+		p.textContent = 'Nie podałeś hasła...';
+		p.style.color = '';
+	}
+};
+
+pass.addEventListener('keyup', checkPassword);
