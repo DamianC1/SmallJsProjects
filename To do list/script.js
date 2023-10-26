98ff28222e27a020346ce08460c4fe80d026c86e
Damian Cygan <damiancygan22@gmail.com>
2023-10-26 23:20:59 +0200

    finished Todo list project

--- To do list/script.js
+++ To do list/script.js
@@ -3,6 +3,14 @@ let errorInfo;
 let addBtn;
 let ulList;
 let newToDo;
+let remove;
+
+let popup; //popup
+let popupInfo; // text w popupie
+let toDoToEdit; // edytowany todo
+let popupInput; // input w popupie
+let popupAddBtn; // przycisk "zatwierdź" w popupie
+let popupCloseBtn; // przycisk "anuluj" w popupie
 
 const main = () => {
 	prepareDomElements();
@@ -13,10 +21,19 @@ const prepareDomElements = () => {
 	errorInfo = document.querySelector('.error-info');
 	addBtn = document.querySelector('.btn-add');
 	ulList = document.querySelector('.toDoList ul');
+	popup = document.querySelector('.popup');
+	popupInfo = document.querySelector('.popup-info');
+	popupInput = document.querySelector('.popup-input');
+	popupAddBtn = document.querySelector('.accept');
+	popupCloseBtn = document.querySelector('.cancel');
+	removeToDoBtn = document.querySelector('.delete');
 };
 const prepareDomEvents = () => {
 	addBtn.addEventListener('click', addNewToDo);
 	ulList.addEventListener('click', checkClick);
+	popupCloseBtn.addEventListener('click', cancelPopup);
+	popupAddBtn.addEventListener('click', changeTodoText);
+	toDoInput.addEventListener('keyup', enterKeyCheck);
 };
 
 const addNewToDo = () => {
@@ -57,10 +74,46 @@ const checkClick = (e) => {
 		e.target.closest('li').classList.toggle('completed');
 		e.target.classList.toggle('completed');
 	} else if (e.target.matches('.edit')) {
-		console.log('edit');
+		editTodo(e);
 	} else if (e.target.matches('.delete')) {
-		console.log('delete');
+		deleteToDo(e);
 	}
+};
+
+const editTodo = (e) => {
+	toDoToEdit = e.target.closest('li');
+	popupInput.value = toDoToEdit.firstChild.textContent;
+	popup.style.display = 'flex';
+};
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		toDoToEdit.firstChild.textContent = popupInput.value;
+		popup.style.display = 'none';
+		popupInfo.textContent = '';
+	} else {
+		popupInfo.textContent = 'Podaj wartość';
+	}
+};
+
+const deleteToDo = (e) => {
+	e.target.closest('li').remove();
+
+	const allTodos = ulList.querySelectorAll('li');
+
+	if (allTodos.length === 0) {
+		errorInfo.textContent = 'Brak zadań na liście';
+	}
+};
+
+const enterKeyCheck = (e) => {
+	if (e.key === 'Enter') {
+		addNewToDo();
+	}
+};
+
+const cancelPopup = () => {
+	popup.style.display = 'none';
 };
 
 document.addEventListener('DOMContentLoaded', main);
