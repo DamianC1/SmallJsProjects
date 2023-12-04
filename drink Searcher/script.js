da925f464b66cf6574134a426a348abe4a48dbfb
Damian Cygan <damiancygan22@gmail.com>
2023-12-04 10:16:43 +0100

    finished project

--- drink Searcher/script.js
+++ drink Searcher/script.js
@@ -1,1 +1,17 @@
 const input = document.querySelector('.search-input');
+const drinks = document.querySelectorAll('li');
+
+const searchEngine = (e) => {
+	const text = e.target.value.toLowerCase();
+
+	drinks.forEach((el) => {
+		const task = el.textContent;
+		if (task.toLowerCase().indexOf(text) !== -1) {
+			el.style.display = 'block';
+		} else {
+			el.style.display = 'none';
+		}
+	});
+};
+
+input.addEventListener('keyup', searchEngine);
