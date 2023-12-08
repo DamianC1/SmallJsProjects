57cf3f3f310825850e99ecf5b67df7f6deb150f0
Damian Cygan <damiancygan22@gmail.com>
2023-12-08 18:47:37 +0100

    finished project

--- bill splitter/script.js
+++ bill splitter/script.js
@@ -6,18 +6,32 @@ const tip = document.querySelector('#tip');
 //INFO
 const error = document.querySelector('.error');
 const cost = document.querySelector('.cost');
+const costInfo = document.querySelector('.cost-info');
 
 //BUTTON
 const count = document.querySelector('.count');
 
 //FUNCTIONS
-const counting = () => {
+
+const showBill = () => {
 	if (price.value == '' || people.value == '' || tip.value == 0) {
 		error.style.display = 'block';
+		costInfo.style.display = 'none';
 	} else {
-		moneyToPay = (price.value + price.value * tip.value) / people.value;
-		console.log(moneyToPay);
+		countBill();
 	}
 };
 
-count.addEventListener('click', counting);
+const countBill = () => {
+	const newPrice = parseFloat(price.value);
+	const newPeople = parseInt(people.value);
+	const newTip = parseFloat(tip.value);
+
+	const toPay = (newPrice + newPrice * newTip) / newPeople;
+
+	cost.textContent = toPay.toFixed(2);
+	costInfo.style.display = 'block';
+	error.style.display = 'none';
+};
+
+count.addEventListener('click', showBill);
