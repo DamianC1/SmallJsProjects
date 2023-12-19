c43e72a4e44149572a901d941a68c90c30b1abc7
Damian Cygan <damiancygan22@gmail.com>
2023-12-19 18:06:20 +0100

    finished project

--- better password validator/script.js
+++ better password validator/script.js
@@ -8,11 +8,24 @@ const sendBtn = document.querySelector('.submit');
 const clearBtn = document.querySelector('.clear');
 const closeBtn = document.querySelector('.close');
 
+// COMPONENTS
+const popup = document.querySelector('.popup');
 // FUNCTIONS
 
 const checkLength = (input, min) => {
 	if (input.value.length < min) {
 		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} składa się minimum z ${min} znaków `);
+	}
+};
+
+const checkEmail = email => {
+	const re =
+		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+	if (re.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, 'Podaj poprawny e-mail');
 	}
 };
 
@@ -34,6 +47,19 @@ const clearError = input => {
 	formBox.classList.remove('error');
 };
 
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box');
+	let errorCount = 0;
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++;
+		}
+	});
+	if (errorCount === 0) {
+		popup.classList.add('show-popup');
+	}
+	console.log(errorCount);
+};
 const checkForm = input => {
 	input.forEach(el => {
 		if (el.value === '') {
@@ -51,6 +77,8 @@ sendBtn.addEventListener('click', e => {
 	checkLength(userName, 3);
 	checkLength(userPassword, 8);
 	passwordWalidator(userPassword, repeatPassword);
+	checkEmail(userEmail);
+	checkErrors();
 });
 
 clearBtn.addEventListener('click', e => {
@@ -58,5 +86,6 @@ clearBtn.addEventListener('click', e => {
 
 	[userName, userPassword, repeatPassword, userEmail].forEach(el => {
 		el.value = '';
+		clearError(el);
 	});
 });
