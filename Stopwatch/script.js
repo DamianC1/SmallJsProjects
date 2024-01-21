cac048e5945257242616428d0ad765d839fce066
Damian Cygan <damiancygan22@gmail.com>
2024-01-21 02:21:39 +0100

    js buttons

---
+++ Stopwatch/script.js
@@ -0,0 +1,10 @@
+//BUTTONS
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const historyBtn = document.querySelector('.start');
+const stopwatchBtn = document.querySelector('.stopwatch');
+
+const time = document.querySelector('.time');
+const timeList = document.querySelector('.time-list');
