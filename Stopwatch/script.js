0e62b5d0d46ac574fbf1ad1f58740cc259e44247
Damian Cygan <damiancygan22@gmail.com>
2024-01-23 22:02:39 +0100

    starting stopwatch js code

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -4,7 +4,32 @@ const pauseBtn = document.querySelector('.pause');
 const stopBtn = document.querySelector('.stop');
 const resetBtn = document.querySelector('.reset');
 const historyBtn = document.querySelector('.start');
-const stopwatchBtn = document.querySelector('.stopwatch');
+const stopwatch = document.querySelector('.stopwatch');
+const infoBtn = document.querySelector('.info');
+const closeModalBtn = document.querySelector('.modal-shadow');
 
 const time = document.querySelector('.time');
 const timeList = document.querySelector('.time-list');
+const modalShadow = document.querySelector('.modalShadow');
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
+const handleStart = () => {
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:0${seconds}`;
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:${seconds}`;
+		} else {
+			minutes++;
+			seconds = 0;
+			stopwatch.textContent = `${minutes}:00`;
+		}
+	}, 1000);
+};
+
+startBtn.addEventListener('click', handleStart);
